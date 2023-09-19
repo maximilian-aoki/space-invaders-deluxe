ea435c2b9542e3741be70efecae0f331c39b7b5c
Maximilian Aoki <135271252+maximilian-aoki@users.noreply.github.com>
2023-09-18 22:40:25 -0400

    Initial commit.

---
+++ main.py
@@ -0,0 +1,13 @@
+from turtle import Turtle, Screen
+import time
+
+# initialize turtle Screen instance
+screen = Screen()
+screen.setup(width=1000, height=1000)
+screen.tracer(0)
+
+
+
+
+# end program
+screen.exitonclick()
