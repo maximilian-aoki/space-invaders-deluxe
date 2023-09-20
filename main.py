5af520355e242f295bfb46c1f21ceac198622cf0
Maximilian Aoki <135271252+maximilian-aoki@users.noreply.github.com>
2023-09-19 20:35:54 -0400

    Order code and make pretty.

--- main.py
+++ main.py
@@ -5,12 +5,16 @@ from barricade_manager import BarricadeManager
 from datetime import datetime
 import time
 
+# ------------------------- INITIALIZE GAME OBJECTS ------------------------- #
 
 # initialize turtle Screen instance
 screen = Screen()
 screen.bgcolor("black")
 screen.setup(width=1200, height=1000)
 screen.tracer(0)
+
+# artificial timer
+start_time = datetime.now()
 
 # initialize gameplay objects
 player = Player()
@@ -23,37 +27,44 @@ screen.onkeypress(fun=player.move_left, key="Left")
 screen.onkeypress(fun=player.move_right, key="Right")
 screen.onkeypress(fun=player.shoot, key="space")
 
-# artificial timer
-start_time = datetime.now()
-
 # initialize all invaders
 invader_manager.create_invaders()
 eliminated_list = []
 
+# --------------------------- GAMEPLAY --------------------------- #
 
-# GAMEPLAY
 game_on = True
 while game_on:
+    # update the player's screen
     screen.update()
     time.sleep(0.1)
 
-    # reset colors after hits
+    # reset colors and eliminated objects after hits
     player.color("white")
     for item in eliminated_list:
         eliminated_list.remove(item)
         item.hideturtle()
 
-    # invaders move and shoot turn
-    time_check = (datetime.now() - start_time).total_seconds()
-    if time_check >= invader_manager.move_time:
-        invader_manager.invaders_turn()
-        start_time = datetime.now()
+    # --------------------------- MOVE TURNS --------------------------- #
 
     # player lasers move
     for laser in player.all_lasers:
         laser.forward(player.laser_speed)
 
-        # if laser hits invader
+    # invaders move and shoot turn (based on asynchronous invader timer)
+    time_check = (datetime.now() - start_time).total_seconds()
+    if time_check >= invader_manager.move_time:
+        invader_manager.invaders_turn()
+        start_time = datetime.now()
+
+    # move all invader's lasers
+    for invader_laser in invader_manager.all_invader_lasers:
+        invader_laser.forward(invader_laser.laser_speed)
+
+    # --------------------------- PLAYER LASER CONDITIONALS --------------------------- #
+
+    # if player laser hits invader
+    for laser in player.all_lasers:
         for invader in invader_manager.all_invaders:
             if laser.distance(invader) <= 23:
                 invader_manager.all_invaders.remove(invader)
@@ -65,9 +76,10 @@ while game_on:
                 invader.color("LightGreen")
                 break
 
-        # if laser hits barricade
+    # if player laser hits barricade
+    for laser in player.all_lasers:
         for block in barricade_manager.all_blocks:
-            if laser.distance(block) <= 20:
+            if laser.distance(block) <= 21:
                 barricade_manager.all_blocks.remove(block)
                 player.all_lasers.remove(laser)
                 laser.hideturtle()
@@ -77,27 +89,29 @@ while game_on:
                 block.color("LightGreen")
                 break
 
-        # if laser flies off-screen
+    # if player laser flies off-screen
+    for laser in player.all_lasers:
         if laser.ycor() >= 500:
-            laser.hideturtle()
             player.all_lasers.remove(laser)
+            laser.hideturtle()
 
-    # INVADER LASER MOVE
-    for invader_laser in invader_manager.all_invader_lasers:
-        invader_laser.forward(invader_laser.laser_speed)
-
-        # if laser collides with player laser
-        for laser in player.all_lasers:
+    # if player laser collides with invader laser
+    for laser in player.all_lasers:
+        for invader_laser in invader_manager.all_invader_lasers:
             if (laser.xcor() >= invader_laser.xcor() - 1) and (laser.xcor() <= invader_laser.xcor() + 1):
                 if laser.ycor() >= invader_laser.ycor():
                     player.all_lasers.remove(laser)
                     invader_manager.all_invader_lasers.remove(invader_laser)
                     laser.hideturtle()
                     invader_laser.hideturtle()
+                    break
 
-        # if laser hits barricade
+    # --------------------------- INVADER LASER CONDITIONALS --------------------------- #
+
+    # if invader laser hits barricade
+    for invader_laser in invader_manager.all_invader_lasers:
         for block in barricade_manager.all_blocks:
-            if invader_laser.distance(block) <= 20:
+            if invader_laser.distance(block) <= 21:
                 barricade_manager.all_blocks.remove(block)
                 invader_manager.all_invader_lasers.remove(invader_laser)
                 invader_laser.hideturtle()
@@ -107,17 +121,22 @@ while game_on:
                 block.color("LightGreen")
                 break
 
-        # if laser hits player
+    # if invader laser hits player
+    for invader_laser in invader_manager.all_invader_lasers:
         if invader_laser.distance(player) <= 25:
             player.color("red")
             player.lives -= 1
             invader_manager.all_invader_lasers.remove(invader_laser)
             invader_laser.hideturtle()
+            break
 
-        # if laser flies off-screen
+    # if invader laser flies off-screen
+    for invader_laser in invader_manager.all_invader_lasers:
         if invader_laser.ycor() <= -500:
             invader_manager.all_invader_lasers.remove(invader_laser)
             invader_laser.hideturtle()
+
+    # --------------------------- ENDGAME CONDITIONALS --------------------------- #
 
     # check if invaders made it to the base
     for invader in invader_manager.all_invaders:
